fd58aec8b05c6a660f11afbb2c5b43f36ec0ba12
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-10 14:18:39 -0700

    Add contains() and it tests

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -3,18 +3,34 @@ const _ = require('../../underbar');
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
     // Your code goes here
+    const nums = [1, 2, 3, 4];
+    expect(_.contains(nums, 2).toBe(true));
   });
 
   it('returns false if an array does not contain the target', () => {
     // Your code goes here
+    const nums = [1, 2, 3, 4];
+    expect(_.contains(nums, 0)).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
     // Your code goes here
+    const songs = {
+      Beatles: 'hey Jude',
+      Queen: 'Bohemian Rhapsody',
+      Metallica: 'One',
+    };
+    expect(_.contains(songs, 'One')).toBe(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
     // Your code goes here
+    const songs = {
+      Beatles: 'hey Jude',
+      Queen: 'Bohemian Rhapsody',
+      Metallica: 'One',
+    };
+    expect(_.contains(songs, 'Beautiful People')).toBe(false);
   });
 
 });
--- underbar.js
+++ underbar.js
@@ -1,42 +1,42 @@
 // Returns the given value. Seems pointless perhaps but see its use below for providing a default, no-op callback.
-const identity = function(val) {
+const identity = function (val) {
   // Your code goes here
 };
 
 // Returns the first n elements of the given array.
-const first = function(array, n = 1) {
+const first = function (array, n = 1) {
   // Your code goes here
 };
 
 // Returns the last n elements of the given array.
-const last = function(array, n = 1) {
+const last = function (array, n = 1) {
   // Your code goes here
 };
 
 // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
-const indexOf = function(array, target, fromIndex=0) {
+const indexOf = function (array, target, fromIndex=0) {
   // Your code goes here
 };
 
-const isArrayLike = function(obj) {
+const isArrayLike = function (obj) {
   // Your code goes here
 };
 
 // The cornerstone of a functional library -- iterate all elements, pass each to a callback function.
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-const each = function(obj, callback=identity) {
+const each = function (obj, callback=identity) {
   // Your code goes here
 };
 
 // Return the results of applying the callback to each element.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-const map = function(obj, callback=identity) {
+const map = function (obj, callback=identity) {
   // Your code goes here
 };
 
 // Return an array of the values o a certain property in the collection.
 // E.g. given an array of people objects, return an array of just their ages.
-const pluck = function(obj, key) {
+const pluck = function (obj, key) {
   // Your code goes here
 };
 
@@ -46,40 +46,42 @@ const pluck = function(obj, key) {
 // is not given, the first element of the collection is used as the initial
 // value. The callback is invoked with four arguments:
 // (accumulator, value, index|key, collection).
-const reduce = function(obj, callback=identity, initialValue) {
+const reduce = function (obj, callback=identity, initialValue) {
   // Your code goes here
 };
 
 // Return true if the object contains the target.
-const contains = function(obj, target) {
+const contains = function (obj, target) {
   // Your code goes here
+  return reduce(obj, (wasFound, item) => {
+    return wasFound || item === target;
+  }, false);
 };
 
 // Return true if all the elements / object values are accepted by the callback.
-const every = function(obj, callback=identity) {
+const every = function (obj, callback=identity) {
   // Your code goes here
 };
 
 // Return true if even 1 element / object value is accepted by the callback.
-const some = function(obj, callback=identity) {
+const some = function (obj, callback=identity) {
   // Your code goes here
 };
 
 // Return an array with all elements / object values that are accepted by the callback.
-const filter = function(obj, callback=identity) {
+const filter = function (obj, callback=identity) {
   // Your code goes here
 };
 
 // Return object without the elements / object valuesthat were rejected by the callback.
-const reject = function(arr, callback=identity) {
+const reject = function (arr, callback=identity) {
   // Your code goes here
 };
 
 // De-duplicates (de-dups) the elements / object values.
-const uniq = function(obj) {
+const uniq = function (obj) {
   // Your code goes here
 };
-
 
 module.exports = {
   contains: contains,
@@ -96,5 +98,5 @@ module.exports = {
   reduce: reduce,
   reject: reject,
   some: some,
-  uniq: uniq
+  uniq: uniq,
 };
