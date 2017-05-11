6763373cc5021f65e26b83706906f48975ca2a1f
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-11 13:08:46 -0700

    Add pluck() tests

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -3,6 +3,12 @@ const _ = require('../../underbar');
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
     // Your code goes here
+    const arr = [
+      { name: 'John', age: 36, },
+      { name: 'Victor', age: 23, },
+      { name: 'Ann', age: 42, },
+    ];
+    expect(_.pluck(arr, 'age')).toEqual([36, 23, 42]);
   });
 
 });
