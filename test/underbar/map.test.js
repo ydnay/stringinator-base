ff2ca7485d93d68fb56c317a1ef3e1103079a87b
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-11 12:57:05 -0700

    Add map() tests

--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -3,5 +3,9 @@ const _ = require('../../underbar');
 describe('map()', () => {
   it('maps every numbers in an array of numbers to their square', () => {
     // Your code goes here
+    const nums = [2, 5, 6, 9];
+    const mapOutputArr = _.map(nums, (element) =>
+      element * element);
+    expect(mapOutputArr).toEqual([4, 25, 36, 81]);
   });
 });
