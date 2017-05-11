aeca5e3c179af0baccc8bfb170d4653cd933f3b5
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-11 14:47:21 -0700

    Add uniq() tests

--- test/underbar/uniq.test.js
+++ test/underbar/uniq.test.js
@@ -3,5 +3,7 @@ const _ = require('../../underbar');
 describe('uniq()', () => {
   it('de-dups a list of numbers', () => {
     // Your code goes here
+    const nums = [1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 5, 5, 5, 5];
+    expect(_.uniq(nums)).toEqual([1, 2, 3, 4, 5]);
   });
 });
