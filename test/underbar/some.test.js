afca048a266a35c5c55cf9ff0bec0c6dd9200444
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-11 14:28:25 -0700

    Add some() tests

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -2,11 +2,17 @@ const _ = require('../../underbar');
 
 describe('some()', () => {
   it('returns true if any number is odd', () => {
-    // Your code goes here
+    // Your code goes here\
+    const nums = [2, 4, 6, 8, 9];
+    const result = _.some(nums, (elem) => elem % 2 === 1);
+    expect(result).toBe(true);
   });
 
   it('returns false if no number is odd', () => {
     // Your code goes here
+    const nums = [2, 4, 6, 8, 200];
+    const result = _.some(nums, (elem) => elem % 2 === 1);
+    expect(result).toBe(false);
   });
 
 });
